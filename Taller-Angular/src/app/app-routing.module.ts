61e90183dbe260f3417b483857204c7d0f8144c0
Evelyn Tatiana Auzay Jurado <eauzayju@pichincha.com>
2023-06-20 10:34:38 -0500

    card user dashboard

--- Taller-Angular/src/app/app-routing.module.ts
+++ Taller-Angular/src/app/app-routing.module.ts
@@ -6,14 +6,19 @@ import { DashboardComponent } from './modules/shared/components/dashboard/dashbo
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
   {
-    path: '', component: LayoutComponent,
+    path: '',
+    component: LayoutComponent,
     children: [
-      { 
-        path: '', redirectTo: 'login', pathMatch: 'full' 
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
       },
-      { 
-        path: 'dashboard', component: DashboardComponent
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
       },
       {
         path: 'clients',
@@ -31,13 +36,15 @@ const routes: Routes = [
       },
       {
         path: 'shift',
-        loadChildren: () => import('@modules/shift/shift.module').then((m) => m.ShiftModule),
-      }
-    ]
+        loadChildren: () =>
+          import('@modules/shift/shift.module').then((m) => m.ShiftModule),
+      },
+    ],
   },
   {
     path: 'rides',
-    loadChildren: () => import('@modules/rides/rides.module').then((m) => m.RidesModule)
+    loadChildren: () =>
+      import('@modules/rides/rides.module').then((m) => m.RidesModule),
   },
 ];
 
