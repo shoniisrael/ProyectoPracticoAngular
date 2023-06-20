17af26453b73d3a8e0605d720eb41ba2bd00a29c
israel <jivillac@espe.edu.ec>
2023-06-20 12:18:02 -0500

    maxetado de dashboard horizontal cards

--- Taller-Angular/src/app/modules/dashboard/components/single-horizontal-card/single-horizontal-card.component.ts
+++ Taller-Angular/src/app/modules/dashboard/components/single-horizontal-card/single-horizontal-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-horizontal-card',
   templateUrl: './single-horizontal-card.component.html',
-  styleUrls: ['./single-horizontal-card.component.scss']
+  styleUrls: ['./single-horizontal-card.component.scss'],
 })
 export class SingleHorizontalCardComponent {
-
+  @Input() colorValue: 'pink' | 'orange' | 'blueberry' | 'green' = 'pink';
 }
